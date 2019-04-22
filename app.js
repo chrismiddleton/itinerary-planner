8f17daafd91674658838fe889881102f5336e551
Chris Middleton <chrisdmiddleton@gmail.com>
2019-04-22 00:43:36 -0400

    Adjust date formatting and styling

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import planTrip from './lib/planTrip.js'
 
-var randomTrip = planTrip();
-var div = document.createElement('div');
-div.innerHTML = randomTrip.toString().split(/\n/g).map(x => `<p>${x}</p>`).join('\n');
-document.body.appendChild(div);
+const randomTrip = planTrip()
+const div = document.createElement('div')
+div.innerHTML = randomTrip.toHtml()
+document.body.appendChild(div)
